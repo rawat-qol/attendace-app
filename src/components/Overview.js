ac42eb64d313326fd5c2bafe59f69b179abf3f75
rawat <rawat@qol-system.co.jpcls>
2023-12-27 17:49:25 +0900

    Updated

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -9,7 +9,7 @@ const Overview = () => {
                 <img className="w-full rounded-lg" src="./images/apple.jpg" />
             </div>
             <div className="w-1/2 p-4">
-                <div className="font-bold text-2xl mb-4">Overview of Apple</div>
+                <div className="font-bold text-2xl mb-4">Overview of An Apple</div>
                 <p className="text-gray-700 text-lg">
                 Apples are crisp, round fruits that come in various colors like red, green, and yellow. They're known for their sweet or sometimes tart taste, depending on the variety. Apples are rich in fiber, vitamins, and antioxidants, making them a healthy snack choice. They are versatile, used in various culinary applications from pies and sauces to being enjoyed fresh off the tree.
                 </p>
